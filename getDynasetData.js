8f8bb8bafa9ff8fb871f3fae84c042fb2739357a
christophe dancel <roropekpek@gmail.com>
2022-10-29 04:35:05 +0200

    add % of tokens in dynasets && tvl

--- getDynasetData.js
+++ getDynasetData.js
@@ -16,13 +16,13 @@ const ETH_DYN_ADDRESS = "0x7bb1A6b19e37028B3aA5c580339c640720E35203";
 //const DYDX_DYN_ADRESS = "0x976a95786DA6f6eE1c0755cCFB9A22adac2BF7B2";
 
 //[address, decimals]
-const USDC_INFO = ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",6] ;
+const USDC_INFO = ["0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",6];
 const WBTC_INFO = ["0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599",8];
 const WETH_INFO = ["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",18];
 
 const id_chat = "-1001772396973";
-const urlEthPriceApi = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
-const urlWbtcPriceApi = "";
+const urlWethPriceApi = "https://api.coingecko.com/api/v3/simple/price?ids=weth&vs_currencies=usd";
+const urlBtcPriceApi = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
 
 const init = async() => {
 
@@ -30,75 +30,108 @@ const init = async() => {
     let nameToken2;
     let balanceToken1;
     let balanceToken2;
+    let ratio1;
+    let ratio2;
+    let tvl;
 
     const dyn_btc_contract = new web3.eth.Contract(BTC_ABI,BTC_DYN_ADDRESS);
     const dyn_eth_contract = new web3.eth.Contract(ETH_ABI,ETH_DYN_ADDRESS);
     
-    const options = { //passer en let au cas ou
+    const options = {
         filter : {
         },
-        fromBlock: 0, // synchro avec dernier block ?
+        fromBlock: 0,
     };
     
     function getName(addressOfToken) {
         if (addressOfToken === USDC_INFO[0] ){
-            return 'USDC'
+            return 'USDC';
         }
         else if (addressOfToken === WBTC_INFO[0]){
-            return 'WBTC'
+            return 'WBTC';
         }
         else if (addressOfToken === WETH_INFO[0]){
-            return 'WETH'
+            return 'WETH';
         }
         else{
-            return `unknown token, at address : ${addressOfToken}`
+            return `unknown token, at address : ${addressOfToken}`;
         }
     }
 
     async function getPrice(url) {
         let response = await fetch(url);
-        response = response.json();
+        return response.json();
     }
 
-    function calculRatio(){
+    async function convertToUsd(token){
 
+        let balance10 = parseInt(token[1]);
+        if (token[0] === 'WETH'){
+            let ethPrice = await getPrice(urlWethPriceApi);
+            ethPrice = ethPrice.weth.usd; 
+            return balance10 * ethPrice;
+        }
+        else if (token[0] === 'WBTC'){
+            let btcPrice = await getPrice(urlBtcPriceApi);
+            btcPrice = btcPrice.bitcoin.usd;
+            return balance10 * btcPrice;
+        }
+        else {
+            return balance10;
+        }
     }
+
+    // async function calculRatio(token1,token2){
+    //     const balanceToken10 = await convertToUsd(token1);
+    //     const balanceToken20 = await convertToUsd(token2);
+    //     const total = balanceToken10 + balanceToken20;
+    //     const ratioToken1 = (balanceToken10 / total)*100;
+    //     const ratioToken2 = (balanceToken20 / total)*100;
+    //     return [ratioToken1.toFixed(2),ratioToken2.toFixed(2),total.toFixed(2)];
+    // }    
     
-    function formatDecimals(amount,token_name){ // parse par 3 les amounts ++ pas aller trop loin après la virgule (pour weth)
+    function formatDecimals(amount,token_name){ 
         amount = parseInt(amount);
         if (token_name === 'USDC'){
             return (amount / 10**USDC_INFO[1]).toFixed(2);
         }
-        if (token_name === 'WBTC'){
+        else if (token_name === 'WBTC'){
             return (amount / 10**WBTC_INFO[1]).toFixed(8);
         }
-        if (token_name === 'WETH'){
+        else if (token_name === 'WETH'){
             return (amount / 10**WETH_INFO[1]).toFixed(8);
         }
         else {
-            return `${amount}, decimals unknown`
+            return `${amount}, decimals unknown`;
         }
     
     }
     
     async function blockToDate(blockNumber) {
         const dateRaw = await web3.eth.getBlock(blockNumber);
-        const date = new Date(dateRaw.timestamp*1000)
+        const date = new Date(dateRaw.timestamp*1000);
         return date.toUTCString();
     }
 
     async function getBalancesContract(dyna){
-        const balances = await dyna.methods.getTokenAmounts().call()
-        const nameToken1 = getName(balances[0][0]);
-        const nameToken2 = getName(balances[0][1]);
-        const balanceToken1 = formatDecimals(balances[1][0],nameToken1);
-        const balanceToken2 = formatDecimals(balances[1][1],nameToken2);
-        return [nameToken1,nameToken2,balanceToken1,balanceToken2];
+        const balances = await dyna.methods.getTokenAmounts().call();
+        const _nameToken1 = getName(balances[0][0]);
+        const _nameToken2 = getName(balances[0][1]);
+        const _balanceToken1 = formatDecimals(balances[1][0],_nameToken1);
+        const _balanceToken2 = formatDecimals(balances[1][1],_nameToken2);
+
+        const _token1 = [_nameToken1, _balanceToken1];
+        const _token2 = [_nameToken2,_balanceToken2];
+        const _UsdBalanceToken1 = await convertToUsd(_token1);
+        const _UsdbalanceToken2 = await convertToUsd(_token2);
+        const _tvl = _UsdBalanceToken1 + _UsdbalanceToken2;
+        const _ratioToken1 = (_UsdBalanceToken1 / _tvl) * 100;
+        const _ratioToken2 = (_UsdbalanceToken2 / _tvl) * 100;
+
+        return [_nameToken1,_nameToken2,_balanceToken1,_balanceToken2, _tvl.toFixed(2), _ratioToken1.toFixed(2), _ratioToken2.toFixed(2)];
     }
 
-
-
-    bot.sendMessage(id_chat,"Bot connected");     // rajouter commande du bot !tvl_eth !tvl_btc OU l'ajouter dans le message du bot
+    bot.sendMessage(id_chat,"Bot connected"); 
 
     async function getDynaData(dyna) { 
         dyna.events.Swap(options)
@@ -110,9 +143,8 @@ const init = async() => {
             const amountOutMin = formatDecimals(event.returnValues.amountOutMin, tokenOut);
             const time = await blockToDate(event.blockNumber);
 
-            [nameToken1,nameToken2,balanceToken1,balanceToken2] = await getBalancesContract(dyna);
-            // faire ratio + afficher TVL (dans commande ?)
-
+            [nameToken1,nameToken2,balanceToken1,balanceToken2, tvl, ratio1, ratio2] = await getBalancesContract(dyna);
+            
             const message = `🚨 🚨 New swap detected 🚨 🚨\n\n${name}:\nSwap ${amountIn} ${tokenIn} for\n${amountOutMin} ${tokenOut}\n\n${name} TVL : ${tvl} $\nAssets:\n  ${balanceToken1} ${nameToken1} (${ratio1}%)\n  ${balanceToken2} ${nameToken2} (${ratio2}%)\n\n${time}`;
             console.log(message);
             bot.sendMessage(id_chat, message);
@@ -120,9 +152,6 @@ const init = async() => {
         } );
     }
    
-//    console.log(await dyn_btc_contract.methods.getTokenAmounts().call())
-
-
     getDynaData(dyn_btc_contract);
     getDynaData(dyn_eth_contract);
 
--- test.js
+++ test.js
@@ -2,19 +2,24 @@ const fetch = require('node-fetch'); // mandatory with nodeJS to work with fetch
 
 
 async function main() {
-    const url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd";
+    const url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
 
     async function test(url) {
         const response = await fetch(url);
         return response.json();
     }
     
-    // test(url).then((data) => console.log(data.ethereum.usd));
+    // test(url).then((data) => console.log(data[0][0]));
     
-    let test4 = await test(url);
-    test4 = test4.ethereum.usd;
-    console.log(test4);
-
+    // let test4 = await test(url);
+    // test4 = test4;
+    // console.log(test4.bitcoin.usd);
+    let num = 1234567890.545425616516516;
+    // num = num.toLocaleString()
+    // num = Math.round(num*1000)/1000;
+    num = num.toLocaleString("en-US",{minimumFractionDigits:6});
+    
+    console.log(num);
 }
 main();
 // async function test2() {
